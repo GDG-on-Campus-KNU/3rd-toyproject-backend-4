c46d7d4a9b3bb0e43a8ccbbe3964b17270515c12
chaejm55 <chaejm55@gmail.com>
2024-01-07 22:00:57 +0900

    Feat: 결제 레포지토리 작성

---
+++ src/main/java/com/example/kiosk/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.example.kiosk.repository;
+
+import com.example.kiosk.domain.payment.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
