518c8f5c74e850b73d3b85dcd02189c58fd267fd
chaejm55 <chaejm55@gmail.com>
2024-01-09 01:21:31 +0900

    Feat: OrderRepository 생성

---
+++ src/main/java/com/example/kiosk/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.kiosk.repository;
+
+import com.example.kiosk.domain.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
