88e4ed8f7058e1b5f08c6b6b8d421c2bf264e946
Koo-EunSung <ku903@naver.com>
2024-01-19 17:31:40 +0900

    Feat: CORS credential 설정 추가

--- src/main/java/com/example/kiosk/CorsConfig.java
+++ src/main/java/com/example/kiosk/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "DELETE", "PATCH");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "DELETE", "PATCH")
+                .allowCredentials(true);
     }
 }
